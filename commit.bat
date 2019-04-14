cff8ec0a10d7b4b0ab4cb8db8a908e89926a2636
GedMarc <ged_marc@hotmail.com>
2019-04-14 02:16:24 +0200

    DevKit and Structure with Cleanup

--- commit.bat
+++ commit.bat
@@ -1,2 +1,2 @@
 git submodule foreach --recursive 'git add . || :'
-git submodule foreach --recursive 'git commit -a -m "DevKit and Cleanup" || :'
+git submodule foreach --recursive 'git commit -a -m "DevKit and Structure with Cleanup" || :'
