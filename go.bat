1f9c35b6140c4dddd02c64236a29485419715503
GedMarc <ged_marc@hotmail.com>
2019-04-16 04:42:32 +0200

    DevKit and Structure with Cleanup

--- go.bat
+++ go.bat
@@ -1,1 +1,1 @@
-mvn compile install -P enable-examples,enable-plugins,enable-jqxwidgets,enabled-guicedee,jdk8,enable-commons,enable-guice,enable-jackson,enable-thirdparty,devkit
+mvn compile install -P enable-examples,enable-plugins,enable-jqxwidgets,enable-guicedee,jdk8,enable-commons,enable-guice,enable-jackson,enable-thirdparty,devkit
--- go_no_tests.bat
+++ go_no_tests.bat
@@ -1,1 +1,1 @@
-mvn compile install -P enable-examples,enable-jqxwidgets,enabled-guicedee,jdk8,enable-commons,enable-guice,enable-jackson,enable-thirdparty,devkit -Dmaven.test.skip-true -DskipTests=true
+mvn compile install -o -P enable-examples,enable-plugins,enable-jqxwidgets,enable-guicedee,jdk8,enable-commons,enable-guice,enable-jackson,enable-thirdparty,devkit -Dmaven.test.skip-true -DskipTests=true
