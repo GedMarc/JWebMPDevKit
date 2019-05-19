1536fa7bccf1a4d18da52282b824c131cf27186c
GedMarc <ged_marc@hotmail.com>
2019-05-19 02:29:54 +0200

    Strictly name cache API, sql server driver Allow java.activation and jakarta.activation modules in one (great going thanks guys ';/') Leave it to the project owners to do the tests (some take forever)

---
+++ go_no_tests_12.bat
@@ -0,0 +1,1 @@
+mvn compile source:jar install -P enable-examples,enable-plugins,enable-jqxwidgets,enable-guicedee,jdk12,enable-commons,enable-guice,enable-jackson,enable-thirdparty,devkit -Dmaven.test.skip-true -DskipTests=true
