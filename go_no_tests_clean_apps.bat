49e19bbe52770e28a0b42be175635dcf4a0e0029
GedMarc <ged_marc@hotmail.com>
2019-05-25 14:10:45 +0200

    Javadoc and Sources for transient module

---
+++ go_no_tests_clean_apps.bat
@@ -0,0 +1,1 @@
+mvn clean compile source:jar install -o -P enable-examples,enable-plugins,enable-jqxwidgets,enable-guicedee,jdk8,enable-commons,enable-guice,enable-jackson,enable-thirdparty,devkit,enable-apps -Dmaven.test.skip-true -DskipTests=true
