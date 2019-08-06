07632876fa598158224ea46194ee9f0bcc32ff11
GedMarc <ged_marc@hotmail.com>
2019-08-06 20:00:46 +0200

    JDK 12 Build and Various

--- go_no_tests_12.bat
+++ go_no_tests_12.bat
@@ -1,1 +1,1 @@
-mvn compile source:jar install -P enable-examples,enable-plugins,enable-jqxwidgets,enable-guicedee,jdk12,enable-commons,enable-guice,enable-jackson,enable-thirdparty,devkit -Dmaven.test.skip-true -DskipTests=true
+mvn compile source:jar install -P enable-examples,enable-plugins,enable-jqxwidgets,enable-guicedee,jdk12,enable-commons,enable-guice,enable-jackson,enable-thirdparty,devkit,enable-apps -Dmaven.test.skip-true -DskipTests=true %1
