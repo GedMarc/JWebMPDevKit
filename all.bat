a6c9ed4bf60229125a51100172bc3403b6810e49
GedMarc <ged_marc@hotmial.com>
2019-12-22 04:00:04 +0200

    Guiced EE Tidying Up and Version Updates

--- all.bat
+++ all.bat
@@ -2,4 +2,4 @@ set /p profilelist=<guicedListAll.txt
 set /p profilelist2=<servicesListAll.txt
 set /p profilelist3=<jwebmpListAll.txt
 
-mvn clean source:jar install -P %profilelist%,%profilelist2%,%profilelist3% -T 8 %1 %2 %3 %4
+mvn clean source:jar install -P %profilelist%,%profilelist2%,%profilelist3%  %1 %2 %3 %4 -DskipTests=true
