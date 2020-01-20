442591a7661db5fd688493bd3e4a32c22e17c437
GedMarc <ged_marc@hotmial.com>
2020-01-20 04:17:07 +0200

    Major Update

---
+++ GuicedEE/Services/java.validation/src/main/java/jakarta/validation/DummyJavaDoc.java
@@ -0,0 +1,6 @@
+package jakarta.validation;
+
+public class DummyJavaDoc
+{
+
+}
--- GuicedEE/Services/java.validation/src/main/java/module-info.java
+++ GuicedEE/Services/java.validation/src/main/java/module-info.java
@@ -1,13 +1,3 @@
 module java.validation {
-	exports javax.validation;
-	exports javax.validation.bootstrap;
-	exports javax.validation.constraints;
-	exports javax.validation.constraintvalidation;
-	exports javax.validation.executable;
-	exports javax.validation.groups;
-	exports javax.validation.metadata;
-	exports javax.validation.spi;
-	exports javax.validation.valueextraction;
 
-	uses javax.validation.spi.ValidationProvider;
 }
