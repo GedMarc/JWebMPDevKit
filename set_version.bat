17434f3c3da464a23afb58d60779549a607380a1
GedMarc <ged_marc@hotmail.com>
2020-11-12 23:09:14 +0200

    BOM Location Updates

--- set_version.bat
+++ set_version.bat
@@ -1,4 +1,4 @@
 set /p profilelist=<servicesListAll.txt
 set /p profilelist2=<guicedListAll.txt
 set /p profilelist3=<jwebmpListAll.txt
-mvn versions:set -DnewVersion=%1 "-DprocessAllModules=true" -P %profilelist%,%profilelist2%,%profilelist3%
+mvn versions:set -DnewVersion=%1 -DprocessAllModules=true -P %profilelist%,%profilelist2%,%profilelist3%
