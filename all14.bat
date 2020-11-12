b457c03a53135aabfde9520bbcfd85ea10a1d04a
GedMarc <ged_marc@hotmail.com>
2020-11-12 21:06:44 +0200

    Jakarta Namespace - jakarta.xml.bind module name -  minor fixes

--- all14.bat
+++ all14.bat
@@ -1,6 +1,6 @@
 set /p profilelist=<Guiced-EE/guicedListAll.txt
 set /p profilelist2=<Guiced-EE/servicesListAll.txt
-# set /p profilelist3=<JWebMP/jwebmpListAll.txt
+REM set /p profilelist3=<JWebMP/jwebmpListAll.txt
 
 set JAVA_HOME=/opt/jdk14
 mvn clean source:jar install -P jdk9,jdk13,jdk14,%profilelist%,%profilelist2%,%profilelist3%  %1 %2 %3 %4 -DskipTests=true  -Djdk.release=14
