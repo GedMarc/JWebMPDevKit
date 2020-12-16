0e49878105a6416229e3018e324ac3a562874325
GedMarc <ged_marc@hotmail.com>
2020-12-16 22:48:36 +0200

    Turn on Type-Safety in JWebMP, Hibernate 5.4.26, JAnsi 2.2.1

--- all14.bat
+++ all14.bat
@@ -3,4 +3,4 @@ set /p profilelist2=<Guiced-EE/servicesListAll.txt
 set /p profilelist3=<JWebMP/jwebmpListAll.txt
 
 set JAVA_HOME=/opt/jdk14
-mvn clean source:jar install -P jdk9,jdk13,jdk14,%profilelist%,%profilelist2%,%profilelist3%  %1 %2 %3 %4 -DskipTests=true  -Djdk.release=14
+mvn clean source:jar  install -P jdk9,jdk13,jdk14,%profilelist%,%profilelist2%,%profilelist3%  %1 %2 %3 %4 -DskipTests=true  -Djdk.release=14
