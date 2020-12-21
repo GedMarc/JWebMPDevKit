4160b13f797d079cddfd9d45359723ea4aaa83da
GedMarc <ged_marc@hotmail.com>
2020-12-21 12:02:49 +0200

    JWebMP Operational Updates and Cleanups

--- JWebMP/jwebmp.bat
+++ JWebMP/jwebmp.bat
@@ -1,2 +1,2 @@
 set /p profilelist=<jwebmpListAll.txt
-mvn clean source:jar install -P %profilelist% %1  %2 %3 %4
+mvn clean source:jar install -P %profilelist% %1  %2 %3 %4 -DskipTests=true -T 6 -o
