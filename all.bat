19f9ab88c54763b50e59abe5381c30dfafebe3c5
GedMarc <ged_marc@hotmail.com>
2021-03-21 04:11:34 +0200

    Hibernate 5.4.30 Guava 30.1.1, CXF 3.4.3, JDK 16 Releases

--- all.bat
+++ all.bat
@@ -2,5 +2,5 @@ set /p profilelist=<Guiced-EE/guicedListAll.txt
 set /p profilelist2=<Guiced-EE/servicesListAll.txt
 set /p profilelist3=<JWebMP/jwebmpListAll.txt
 
-set JAVA_HOME=/opt/jdk15
-mvn clean source:jar install -P jdk9,jdk13,jdk14,jdk15,%profilelist%,%profilelist2%,%profilelist3%  %1 %2 %3 %4 -DskipTests=true -Djdk.release=15
+set JAVA_HOME=/opt/jdk16
+mvn clean source:jar install -P jdk9,jdk13,jdk14,jdk15,jdk16,%profilelist%,%profilelist2%,%profilelist3%  %1 %2 %3 %4 -DskipTests=true -Djdk.release=16
