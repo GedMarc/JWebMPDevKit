d49adbbf7fc843c2999c87e335b0402fd907b0c8
GedMarc <marc.magon@gmail.com>
2021-04-20 05:18:44 +0200

    Force refresh on servlet api

--- all.bat
+++ all.bat
@@ -3,4 +3,4 @@ set /p profilelist2=<Guiced-EE/servicesListAll.txt
 set /p profilelist3=<JWebMP/jwebmpListAll.txt
 
 set JAVA_HOME=/opt/jdk16
-mvn clean source:jar install -P jdk9,jdk13,jdk14,jdk15,jdk16,%profilelist%,%profilelist2%,%profilelist3%  %1 %2 %3 %4 -DskipTests=true -Djdk.release=16
+mvn clean source:jar javadoc:jar install -P jdk9,jdk13,jdk14,jdk15,jdk16,%profilelist%,%profilelist2%,%profilelist3%  %1 %2 %3 %4 -DskipTests=true -Djdk.release=16
