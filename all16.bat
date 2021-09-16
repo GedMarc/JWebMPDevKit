52cfb2e94596671cee9735e378504603441da02d
GedMarc <marc.magon@gmail.com>
2021-09-16 21:08:41 +0200

    JDK 17 with superclass access checks fixes

---
+++ all16.bat
@@ -0,0 +1,12 @@
+set /p profilelist=<Guiced-EE/guicedListAll.txt
+set /p profilelist2=<Guiced-EE/servicesListAll.txt
+set /p profilelist3=<JWebMP/jwebmpListAll.txt
+
+set JAVA_HOME=/opt/jdk16
+
+cd Guiced-EE/BOM/Versioner
+mvn clean source:jar javadoc:jar install -P jdk9,jdk13,jdk14,jdk15,jdk16,%profilelist%,%profilelist2%,%profilelist3%  %1 %2 %3 %4 -DskipTests=true -Djdk.release=16
+cd ..
+cd ..
+cd ..
+mvn clean source:jar javadoc:jar install -P jdk9,jdk13,jdk14,jdk15,jdk16,%profilelist%,%profilelist2%,%profilelist3%  %1 %2 %3 %4 -DskipTests=true -Djdk.release=16
